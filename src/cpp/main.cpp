dac29ba47ff9878ba226ae21f52a880cdcd44694
PicotRodolphe <picot.rodolphe@gmail.com>
2016-07-04 14:07:43 +0200

    test

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -7,13 +7,9 @@ using namespace std;
 
 int main(int argc, char const *argv[]){
 
-	//array<int, n> test {3,2,1};
-
+	
 	init_maho();
 
-	//cout << maho_suiv(4,8) << endl ;
-
-	//affiche_maho();
 
 	cout << chaines_max_bis() << endl ;
 
